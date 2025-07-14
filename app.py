5140b8aed2c0cb87d06278b0bff1b623503c499b
Matthew Boubin <mjb9353@nyu.edu>
2025-07-14 09:04:44 -0400

    added health check

--- app.py
+++ app.py
@@ -28,6 +28,10 @@ def webhook():
                 }]
             }
         })
+    
+@app.route("/health", methods=["GET"])
+def health_check():
+    return jsonify({"status": "Connection Valid"}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
